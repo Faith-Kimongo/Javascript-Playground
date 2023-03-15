4a36bae9cf6a8a515866cad1268c261de80bb061
Faith-Kimongo <faithvictor78@gmail.com>
2023-03-15 10:52:20 +0300

    CV submit button created and CV name html class changed

--- resources/views/profile/main.blade.php
+++ resources/views/profile/main.blade.php
@@ -339,7 +339,7 @@
                               <p class="text-xs text-gray-500">PDF to 10MB</p>
                           </div>
                       </div>
-                      <button type="submit" class="mt-3 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-gradient-to-r from-pink-800 to-pink-900 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-pink-700 hover:to-pink-900">Submit</button>
+                      <button type="submit" class="mt-3 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-gradient-to-r from-pink-800 to-pink-900 bg-origin-border px-4 py-2 text-base font-medium text-white shadow-sm hover:from-pink-700 hover:to-pink-900 ml-5">Submit</button>
                   </form>
                   </div>
                 </div>
